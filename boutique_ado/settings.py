956d6ed9bee10402ad8a21533b23804b336fc589
Josh Hawes <joshua.m.hawes@gmail.com>
2024-04-14 11:57:21 +0000

    edited allowed hosts for deployment to Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-hawesjm-boutiqueadov1-r75npia2p64.ws-eu110.gitpod.io', 'hawesjm-boutique-ado-v1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-hawesjm-boutiqueadov1-r75npia2p64.ws-eu110.gitpod.io', 'https://hawesjm-boutique-ado-v1-5b3a23dd1d30.herokuapp.com', 'localhost']
 
 
 # Application definition
